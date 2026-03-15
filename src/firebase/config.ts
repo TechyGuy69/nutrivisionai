1d89038aea251c831637de2a572e4cc6a4c2e4b2
Usnish Banerjee <banerjeeusnish2@gmail.com>
2026-03-15 09:41:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5474089759-60b73",
+  "appId": "1:83988866566:web:56f0e9326e26612bc40587",
+  "apiKey": "AIzaSyDKR1iPjXxBbxbICuBNqDQj39n65ndWtOk",
+  "authDomain": "studio-5474089759-60b73.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "83988866566"
+};
